57db2c82e55428996b21879e986c78acccfe7fbf
Olawale Oyeyipo <olaclassik@gmail.com>
2022-05-02 13:58:06 +0100

    social links cleanup, doc; deployed

--- oyeyipo.py
+++ oyeyipo.py
@@ -1,14 +1,19 @@
+#!/usr/env/bin python
+"""
+Simple bio package for Oyeyipo Olawale <oyeyipoos@gmail.com>
+"""
 from collections import namedtuple
 
-__version__ = '0.0.1dev0'
+__version__ = "0.0.1dev0"
 
 fname = "Olawale"
 lname = "Oyeyipo"
 full_name = "%s %s" % (fname, lname)
 
-_Rec = namedtuple("Social", "Stackoverflow Github LinkedIn")
-social = _Rec(
-    stackoverflow="www.stackoverflow.com",
-    github="www.github.com/oyeyipo",
-    linkedin="linkedin.com/oyeyipo",
+_Rec = namedtuple("Socials", "stackoverflow github linkedin")
+
+socials = _Rec(
+    stackoverflow="https://stackoverflow.com/users/7024331/oyeyipo",
+    github="https://github.com/oyeyipo",
+    linkedin="https://www.linkedin.com/in/oyeyipowale/",
 )
